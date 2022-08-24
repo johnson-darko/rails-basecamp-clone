a7d9f97edd85575bf557ce4f32674b2f17e934b7
johnson-darko <johnsondarko365@gmail.com>
2022-08-24 22:54:36 +0200

    done db

---
+++ db/migrate/20220824204937_create_project_users.rb
@@ -0,0 +1,14 @@
+class CreateProjectUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :project_users do |t|
+      t.string :project_user_id
+      t.references :project, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.boolean :read_access
+      t.boolean :write_access
+      t.boolean :update_access
+      t.boolean :delete_access
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_201720) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_205250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "message_id"
+    t.bigint "user_id", null: false
+    t.bigint "project_id", null: false
+    t.bigint "project_thread_id", null: false
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_messages_on_project_id"
+    t.index ["project_thread_id"], name: "index_messages_on_project_thread_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "project_threads", force: :cascade do |t|
+    t.string "thread_id"
+    t.bigint "project_id", null: false
+    t.bigint "user_id", null: false
+    t.string "topic"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_project_threads_on_project_id"
+    t.index ["user_id"], name: "index_project_threads_on_user_id"
+  end
+
+  create_table "project_users", force: :cascade do |t|
+    t.string "project_user_id"
+    t.bigint "project_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "read_access"
+    t.boolean "write_access"
+    t.boolean "update_access"
+    t.boolean "delete_access"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_project_users_on_project_id"
+    t.index ["user_id"], name: "index_project_users_on_user_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "projectid"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "task_id"
+    t.string "title"
+    t.bigint "project_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +91,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_201720) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "project_threads"
+  add_foreign_key "messages", "projects"
+  add_foreign_key "messages", "users"
+  add_foreign_key "project_threads", "projects"
+  add_foreign_key "project_threads", "users"
+  add_foreign_key "project_users", "projects"
+  add_foreign_key "project_users", "users"
+  add_foreign_key "projects", "users"
+  add_foreign_key "tasks", "projects"
+  add_foreign_key "tasks", "users"
 end
