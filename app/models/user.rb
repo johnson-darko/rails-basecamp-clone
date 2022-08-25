eeef77c0824528a56972329d259c23afa603463b
johnson-darko <johnsondarko365@gmail.com>
2022-08-25 09:47:12 +0200

    done with lin ks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
   has_secure_password
   has_many :projects, :dependent => :destroy
   has_many :tasks, :dependent => :destroy
@@ -11,5 +6,4 @@ class User < ApplicationRecord
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :userid, :fullname, presence: true, length: {minimum: 5}
   validates :password, presence: true, length: {minimum: 5}, on: :create
-
 end
